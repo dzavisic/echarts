7eeaca277fcf51319681b4b695d2c8eb78edd6f6
zavovkci <dakizavo@gmail.com>
2022-07-08 15:46:58 +0200

    feat(chart): add chart component

---
+++ src/app/components/chart/chart.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from '@angular/core';
+import * as echarts from 'echarts';
+import {EChartsOption, SeriesOption} from 'echarts';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss']
+})
+export class ChartComponent implements OnInit {
+
+  types: any = [
+    'line',
+    'bar',
+    'scatter',
+    'pie',
+    'tree',
+    'gauge',
+    'funnel',
+    'effectScatter',
+    'pictorialBar',
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    const chartDom = document.getElementById('line') as HTMLElement;
+
+    const chart = echarts.init(chartDom);
+
+    const option: EChartsOption = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series:
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        },
+    };
+
+
+    chart.setOption(option);
+
+    let i = 0;
+
+    setInterval(() => {
+      const series = option.series as SeriesOption;
+      i = (i === this.types.length - 1) ? 0 : i + 1;
+      series.type = this.types[i];
+
+      chart.setOption(option);
+    }, 2000);
+
+    window.onresize = function () {
+      chart.resize();
+    };
+  }
+
+}
